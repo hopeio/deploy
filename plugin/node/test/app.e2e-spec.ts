fd39e6ed03be3c8bc98dca28df4196dfe69c2e0d
贾一饼 <lby.i@qq.com>
2024-05-31 00:00:16 +0800

    fix: node plugin

--- plugin/node/test/app.e2e-spec.ts
+++ plugin/node/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../convert/app.module';
+import { AppModule } from './../src/drone/convert/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
