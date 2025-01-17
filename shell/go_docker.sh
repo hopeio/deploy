e7c28917e95d7c8fac1308df0a3803b74f370423
liov <lby.i@qq.com>
2025-01-17 22:56:03 +0800

    update bash

--- shell/go_docker.sh
+++ shell/go_docker.sh
@@ -25,7 +25,8 @@ echo GOOS=linux go build -trimpath -o  build/"$1" "$2"
 GOOS=linux go build -trimpath -o  build/"$1" "$2"
 cmd="\"./$1\",\"-c\",\"./config/$1.toml\""
 dockerfilepath=build/Dockerfile
-rundir=$(dirname $0)
+rundir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+echo $rundir
 source ${rundir}/dockerfile.sh $dockerfilepath $1 $cmd $register
 
 #docker run --rm -v $GOPATH:/go -v $PWD:/work -w /work -e GOPROXY=$GOPROXY $GOIMAGE go build  -trimpath -o /work/build/$output /work/$1
--- shell/win_go_docker.sh
+++ shell/win_go_docker.sh
@@ -23,7 +23,7 @@ fi
 GOOS=linux go build -trimpath -o  build/"$1" "$2"
 cmd="\"./$1\",\"-c\",\"./config/$1.toml\""
 dockerfilepath=build/Dockerfile
-rundir=$(dirname $0)
+rundir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 echo $rundir
 source ${rundir}/dockerfile.sh $dockerfilepath $1 $cmd $register
 
