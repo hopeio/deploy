b2e868a371d2f5fd0a0e56461a3061043c871d49
贾一饼 <lby.i@qq.com>
2025-12-23 19:30:42 +0800

    update

--- plugin/drone/convert/convert.go
+++ plugin/drone/convert/convert.go
@@ -6,13 +6,12 @@ import (
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/converter"
 	httpx "github.com/hopeio/gox/net/http"
-	"github.com/hopeio/gox/net/http/binding"
 )
 
 func main() {
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var req converter.Request
-		binding.Bind(r, &req)
+		httpx.Bind(r, &req)
 		// TODO:
 		httpx.RespondSuccess(r.Context(), w, &drone.Config{
 			Data: "",
